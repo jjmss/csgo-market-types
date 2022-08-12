b0762b044f5165e92ed4db89fab36a6b3c3665d1
Jon Martin Stuberg <jm.stuberg00@gmail.com>
2022-08-13 01:13:20 +0200

    Added SCOPES and PUBLICATION_STATE + documentation

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * @description Different level of warnings
+ * @since 1.0.0
+ */
 exports.WARNING_LEVELS = {
 	CRITICAL: 1,
 	ERROR: 2,
@@ -5,10 +9,62 @@ exports.WARNING_LEVELS = {
 	INFO: 4,
 };
 
+/**
+ * @description Where the warning should be displayed
+ * @since 1.0.0
+ */
 exports.WARNING_TYPES = {
 	USER_MESSAGE: 1,
 	SITE_MESSAGE: 2,
 	SETTINGS_PAGE: 3,
 	ITEM_PAGE: 4,
 	ACCOUNT_PAGE: 5,
+};
+
+/**
+ * @description Authentication scopes
+ * @since 1.0.2
+ */
+exports.SCOPES = {
+	READ: {
+		USER: "read:user",
+		USERS: "read:users",
+		LISTINGS: "read:listings",
+		OTHERS_CONVERSATIONS: "read:othersConversations",
+	},
+	CREATE: {
+		CONVERSATION: "create:conversation",
+		LISTING: "create:listing",
+		MESSAGE: "create:message",
+	},
+	EDIT: {
+		LISTING: "edit:listing",
+		LISTINGS: "edit:listings",
+		PROFILE: "edit:profile",
+	},
+	DELETE: {
+		LISTING: "delete:listing",
+		LISTINGS: "delete:listings",
+	},
+};
+
+/**
+ * @description Different publication states for listings
+ * @since 1.0.2
+ */
+exports.PUBLICATION_STATE = {
+	/**
+	 * @description Everyone can see it
+	 */
+	PUBLIC: 0,
+
+	/**
+	 * @description Only people with the link can see it
+	 */
+	PRIVATE: 1,
+
+	/**
+	 * @description Only you can see it
+	 */
+	UNLISTED: 2,
 };
